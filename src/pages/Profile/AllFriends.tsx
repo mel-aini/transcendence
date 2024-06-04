d8edd6ef3703fcd9e2821828dfb2b4a94fd44157
Othman Chouikhane <ochouikh@e3r5p3.1337.ma>
2024-06-04 16:58:48 +0100

    save

--- src/pages/Profile/AllFriends.tsx
+++ src/pages/Profile/AllFriends.tsx
@@ -1,13 +1,26 @@
-import play_icon from "/play_icon.svg"
-import send_icon from "/send_icon.svg"
-import more_icon from "/more_icon_white.svg"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import axios from "axios"
 import { UserData } from "../../types/profile"
+import FriendBar from "./FriendBar"
+import { context } from "./Friends"
 
 const AllFriends = () => {
 
 	const [friends, setFriends] = useState<UserData | null>(null);
+	const [relation, setRelation] = useState<string>("friend");
+	const seeMore = useContext(context);
+
+	const HandleFriend = () => {
+		setRelation("friend");
+	}
+	
+	const HandlePending = () => {
+		setRelation("rec_inv");
+	}
+	
+	const HandleBlocked = () => {
+		setRelation("blocked");
+	}
 
 	const getFriends = async () => {
 		try {
@@ -23,40 +36,66 @@ const AllFriends = () => {
 	}, []);
 
 	return (
-		<div className="flex flex-col justify-between gap-5 sm:py-10 sm:px-16 p-7 border border-primary rounded-[10px] absolute max-w-[652px] w-[85%] max-h-[840px] bg-gray3">
-			<span className="self-end text-sm text-secondary">close</span>
-			<div className="flex justify-between max-w-[268px] w-full">
-				<span className="text-primary font-medium border-b border-primary pb-3">Friends</span>
-				<span className="font-normal">Pending</span>
-				<span className="font-normal">Blocked</span>
-			</div>
-			{/* <div> */}
-				<input type="text" placeholder="search" className="bg-transparent border-b-[0.5px] w-full px-3 py-1 font-thin" />
-			{/* </div> */}
-				{friends && friends.map((friend: UserData, key: number) => {
-					return (
-							<div className="flex flex-col justify-between items-center w-full">
-								<div key={key} className="flex justify-between items-center w-full h-[70px]">
-									<div className="flex justify-between items-center gap-2 h-[50px]">
-										<img src={friend.profile_image} alt={friend.profile_image} width={50} height={50} className="rounded-full overflow-hidden"/>
-										<span className="shrink overflow-hidden text-ellipsis">{friend.username}</span>
-									</div>
-									<div className="flex justify-between sm:gap-[27px]">
-										<div className="flex gap-[0.5px]">
-											<img src={more_icon} alt="" width={5.36} height={5.36}/>
-											<img src={more_icon} alt="" width={5.36} height={5.36}/>
-											<img src={more_icon} alt="" width={5.36} height={5.36}/>
-										</div>
-										<img src={play_icon} alt="" width={20} height={20}/>
-										<img src={send_icon} alt="" width={20} height={20}/>
-									</div>
-								</div>
-								<hr className="w-[77%] self-center"/>
+		<>
+		{
+			seeMore.seeAll &&
+			<div className="relative">
+				<div className="fixed top-0 start-0 bg-black opacity-70 size-full" onClick={() => seeMore.setSeeAll(false)}/>
+				<div className="z-10 flex flex-col justify-between gap-6 pt-[30px] pb-[58px] sm:px-16 px-8 fixed top-10 left-[50%] -translate-x-1/2 border border-primary rounded-[10px] max-w-[652px] w-[85%] bg-gray3">
+					<span className="self-end text-sm text-secondary" onClick={() => seeMore.setSeeAll(false)}>close</span>
+					<div className="flex justify-between max-w-[268px] w-full gap-2">
+						{
+							relation === "friend" ?
+							<div onClick={HandleFriend} className="h-[36px] w-[59px] flex flex-col justify-between items-center">
+								<span className="text-primary font-medium">Friends</span>
+								<div className="border-b border-primary w-full" />
 							</div>
-						)
-					})
-				}
-		</div>
+							:
+							<div onClick={HandleFriend} className="h-[36px] w-[59px] flex flex-col justify-between items-center">
+								<span className="font-normal">Friends</span>
+								<div className="border-b border-primary w-full" />
+							</div>
+						}
+						{
+							relation === "rec_inv" ?
+							<div onClick={HandlePending} className="h-[36px] w-[67px] flex flex-col justify-between items-center">
+								<span className="text-primary font-medium">Pending</span>
+								<div className="border-b border-primary w-full" />
+							</div>
+							:
+							<div onClick={HandlePending} className="h-[36px] w-[67px] flex flex-col justify-between items-center">
+								<span className="font-normal">Pending</span>
+								<div className="border-b border-primary w-full" />
+							</div>
+						}
+						{
+							relation === "blocked" ?
+							<div onClick={HandleBlocked} className="h-[36px] w-[63px] flex flex-col justify-between items-center">
+								<span className="text-primary font-medium">Blocked</span>
+								<div className="border-b border-primary w-full" />
+							</div>
+							:
+							<div onClick={HandleBlocked} className="h-[36px] w-[63px] flex flex-col justify-between items-center">
+								<span className="font-normal">Blocked</span>
+								<div className="border-b border-primary w-full" />
+							</div>
+						}
+					</div>
+					{/* <div> */}
+						<input type="text" placeholder="search" className="bg-transparent border-b-[0.5px] w-full px-3 pb-[9px] font-thin" />
+					{/* </div> */}
+						{friends && friends.map((friend: UserData, key: number) => {
+							return (
+								<div key={key}>
+									<FriendBar friend={friend} relation={relation}/>
+								</div>
+							)
+						})
+					}
+				</div>
+			</div>
+		}
+		</>
 	)
 }
 
---
+++ src/pages/Profile/FriendBar.tsx
@@ -0,0 +1,59 @@
+import play_icon from "/play_icon.svg"
+import send_icon from "/send_icon.svg"
+import more_icon from "/more_icon_white.svg"
+import accept from "/accept.svg"
+import deny from "/deny.svg"
+import { UserData } from "../../types/profile"
+
+const Action = ({relation}: {relation: string}) => {
+	return (<>
+		{
+			(relation === "friend") && 
+			<div className="flex justify-end gap-[27px] shrink-0">
+				<div className="flex gap-[0.5px]">
+					<img src={more_icon} alt="" width={5.36} height={5.36}/>
+					<img src={more_icon} alt="" width={5.36} height={5.36}/>
+					<img src={more_icon} alt="" width={5.36} height={5.36}/>
+				</div>
+				<img src={play_icon} alt="" width={20} height={20}/>
+				<img src={send_icon} alt="" width={20} height={20}/>
+			</div>
+		}
+		{
+			(relation === "rec_inv") && 
+			<div className="flex justify-end gap-[30px] shrink-0">
+				<img src={deny} alt="" width={20}/>
+				<img src={accept} alt="" width={20}/>
+			</div>
+		}
+		{
+			(relation === "blocked") && 
+			<div className="flex justify-center shrink-0 w-[101px] h-[26px] rounded-[15px] border border-secondary text-secondary">
+				unblock
+			</div>
+		}
+		</>
+	)
+}
+
+const FriendBar = ({friend, relation}: {friend: UserData, relation: string}) => {
+	return (
+		<>
+		{
+			(friend.relation === relation) &&
+			<div className="flex flex-col justify-between items-center w-full">
+				<div className="flex justify-between items-center w-full gap-3 h-[70px]">
+					<div className="flex justify-between items-center gap-4">
+							<img src={friend.profile_image} alt={friend.profile_image} width={50} height={50} className="rounded-full overflow-hidden shrink-0"/>
+							<span className="shrink overflow-hidden text-ellipsis">{friend.username}</span>
+					</div>
+					<Action relation={relation} />
+				</div>
+				<div className="w-[77%] self-center border-b-[0.5px] border-b-white opacity-50"/>
+			</div>
+		}
+		</>
+	)
+}
+
+export default FriendBar;
