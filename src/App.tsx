4c8c4939a1093a74a67a2813014c13377b61fd94
Mohssine El Aini <elaini.mohssine@gmail.com>
2024-06-11 19:30:16 +0100

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,55 @@
 import { Route, Routes } from "react-router-dom";
-import { default as SignUp } from "./pages/Sign-up/Index";
-import { default as Login } from "./pages/Login/Index"; 
-import { default as Home } from "./pages/Home/Index";
-import { default as Chat } from "./pages/Chat/Index";
-import { default as Game } from "./pages/PingPong/Index";
-import { default as Settings } from "./pages/Settings/Index";
-import { default as Profile } from "./pages/Profile/Index";
-import { default as Dashboard } from "./pages/Dashboard/Index";
 import Loading from "./components/Loading";
 import GlobalContextProvider from "./contexts/store";
 import Layout from "./components/Layout";
 import MainLayout from "./MainLayout";
 import ChatContextProvider from "./contexts/chatStore";
-
-import { default as PingPong } from './pages/PingPong/Index'
-import { default as Play } from './pages/PingPong/Play/Index'
 import PingPongContextProvider from "./contexts/pingPongStore";
+import { Suspense, lazy } from "react";
+import LoadingPage from "./components/LoadingPage";
+
+const Home = lazy(() => import('./pages/Home/Index'));
+const Chat = lazy(() => import('./pages/Chat/Index'));
+const Login = lazy(() => import('./pages/Login/Index'));
+const SignUp = lazy(() => import('./pages/Sign-up/Index'));
+const Profile = lazy(() => import('./pages/Profile/Index'));
+const Settings = lazy(() => import('./pages/Settings/Index'));
+const Dashboard = lazy(() => import('./pages/Dashboard/Index'));
+const PingPong = lazy(() => import('./pages/PingPong/Index'));
+const Play = lazy(() => import('./pages/PingPong/Play/Index'));
 
 function App() {
   return (
       <GlobalContextProvider>
         <ChatContextProvider>
           <PingPongContextProvider>
-            <Loading />
-            <Routes>
-              <Route path="/" element={<MainLayout />}>
-                <Route index element={<Home />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route element={<Layout />}>
-                  <Route path="/game" element={<Game />} />
-                  <Route path="/settings" element={<Settings />} />
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path='/users'>
-                    <Route path=':id' element={<Profile />} />
-                    <Route path='*' element={<>Not Found</>} />
+            <Suspense fallback={<LoadingPage />}>
+              <Loading />
+              <Routes>
+                <Route path="/" element={<MainLayout />}>
+                  <Route index element={<Home />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="loading" element={<LoadingPage />}/>
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/chat" element={<Chat />} />
+                  <Route element={<Layout />}>
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path='/users'>
+                      <Route path=':id' element={<Profile />} />
+                      <Route path='*' element={<>Not Found</>} />
+                    </Route>
+                    <Route path='/ping-pong'>
+                      <Route index element={<PingPong />} />
+                      <Route path='play' element={<Play />} />
+                      <Route path='*' element={<>Not Found</>} />
+                    </Route>
                   </Route>
-                  <Route path='/ping-pong'>
-                    <Route index element={<PingPong />} />
-                    <Route path='play' element={<Play />} />
-                    <Route path='*' element={<>Not Found</>} />
-                  </Route>
+                  <Route path='*' element={<>Not Found</>} />
                 </Route>
-                <Route path='*' element={<>Not Found</>} />
-              </Route>
-            </Routes>
+              </Routes>
+            </Suspense>
           </PingPongContextProvider>
         </ChatContextProvider>
       </GlobalContextProvider>
