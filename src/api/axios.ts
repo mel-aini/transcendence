18734b2b46bb712d805ce126f952ace7e2e531ec
Mohssine El Aini <elaini.mohssine@gmail.com>
2024-07-04 14:09:45 +0100

    save

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
 	baseURL: 'http://127.0.0.1:8000',
-	timeout: 1000
+	timeout: 1000,
+	withCredentials: true,
 })
 
 export default api;
