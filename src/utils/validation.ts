869daca516f068110231419b7eeb922e0b57b2c6
Othmane Chouikhane <othman-chouikhane@outlook.fr>
2024-10-15 18:18:36 +0100

    update password regex

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -7,7 +7,7 @@ interface Pattern {
 const patterns: Pattern = {
 	username: /^[a-z][\w-]{2,15}[a-z\d]$/,
 	email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-	password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
+	password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 }
 
 const validate = (type: 'username' | 'email' | 'password', input: string | undefined) : boolean => {
